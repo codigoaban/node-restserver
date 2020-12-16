90782c92061ce0fa586760444dd062997ff96dc5
Gladys <codigoaban@gmail.com>
2020-12-16 16:57:13 -0500

    Primer commit

---
+++ server/config/config.js
@@ -0,0 +1,5 @@
+//===========================
+//Puerto configuracion
+//===========================
+
+process.env.PORT = process.env.PORT || 3000; //le asiga el puerto que encuentre en produccion, si no el puerto 3000(en desarrollo)
