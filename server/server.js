13e6da3f026157a0fa33faf0a957ffe8a11e756e
Gladys <codigoaban@gmail.com>
2020-12-19 15:58:49 -0500

    Restserver - usuario

---
+++ server/server.js
@@ -0,0 +1,55 @@
+require('./config/config');
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+app.use(require('./routes/usuario'));
+
+
+
+//cafe nombre de base de datos
+
+
+//conexion local
+/* mongoose.connect('mongodb://localhost:27017/cafe', { useNewUrlParser: true, useCreateIndex: true },
+    (err, res) => {
+        if (err) throw err;
+        console.log("Base de datos online");
+    }); */
+
+
+
+
+//const MongoClient = require('mongodb').MongoClient; // para la capa free
+
+mongoose.connect(process.env.URLDB, { useNewUrlParser: true }, (err, res) => {
+    if (err) throw err;
+    console.log("Base de datos online");
+});
+
+
+
+/*
+//ok, conexion
+mongoose.connect(process.env.URLDB).then(() => {
+    console.log("Base de datos conectada");
+}).catch((err) => {
+    console.log("No se pudo establecer la conexion a la BD ERROR! ", err);
+});*/
+
+
+
+
+app.listen(process.env.PORT, () => {
+    console.log('Escuchando puerto: ', process.env.PORT);
+});
