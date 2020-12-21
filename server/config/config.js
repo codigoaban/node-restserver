9982ae3729caaa13e485903a5e87282904e0c7ad
Gladys <codigoaban@gmail.com>
2020-12-21 11:04:52 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb+srv://userMongo:slhxMlv0nGa6GzdP@cluster0.hd5o1.mongodb.net/cafe';
+    urlDB = MONGO_URI;
 }
 
 //process.env.URLDB = urlDB;
